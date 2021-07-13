bf81529096523e29115a608c12d57800a98ab9f2
eswaribala <parameswaribala@gmail.com>
2021-07-13 11:01:11 +0530

    updated

---
+++ productapi/src/test/java/com/eric/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
